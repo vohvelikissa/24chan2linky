1348cd5ec9370f1b6eacb82c56ba78189b3c5c8f
Pessi <--add>
2024-08-25 20:54:50 +0300

    finished this shit for now

--- blindluck.sh
+++ blindluck.sh
@@ -11,3 +11,12 @@ cat grabbedlinks.txt >> combination.txt
 cat combination.txt | grep "github" > nc2.txt
 cat combination.txt | grep "pastebin" >> nc2.txt
 cat nc2.txt | sort -u > nc3.txt
+rm grabbedlinks.txt
+rm combination.txt
+rm nc2.txt
+mv nc3.txt grabbedlinks.txt
+cat grabbedlinks.txt | cut -b 7-90 > g2.txt
+rm grabbedlinks.txt
+mv g2.txt grabbedlinks.txt
+w3m $(cat grabbedlinks.txt)
+rm grabbedlinks.txt
